795a22a89ff1ff3f172a0cf89f7605df6b3fc88d
Greyson Barber <barbergn@mail.uc.edu>
2024-11-14 09:10:51 -0500

    made changes

--- dataAnomoliesPackage/dataAnomolies.py
+++ dataAnomoliesPackage/dataAnomolies.py
@@ -1,20 +1,18 @@
-#data Anomolies
+#data Anomolies.py
 
 import pandas as pd
  
 class AnomalyHandler:
-    def __init__(self, csv_path):
+    def __init__(self, csv_path) :
         self.csv_path = csv_path
  
     def process_anomalies(self):
-        # Load data and identify anomalies
-        df = pd.read_csv(self.csv_path)
-        anomalies = df[df['Fuel Type'] == 'Pepsi']  # Rows with Pepsi
- 
-        # Save anomalies to dataAnomalies.csv
+        # Set low_memory to False to avoid mixed dtype warnings
+        df = pd.read_csv(self.csv_path, low_memory=False)
+        anomalies = df[df['Product'] == 'Pepsi']  # Rows with Pepsi
         anomalies.to_csv('data/dataAnomalies.csv', index=False)
         # Filter out anomalies and retain only relevant rows
-        self.cleaned_df = df[df['Fuel Type'] != 'Pepsi']
+        self.cleaned_df = df[df['Product'] != 'Pepsi']
 
 
 
